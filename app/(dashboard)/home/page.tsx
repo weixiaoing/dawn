d797710401ad9e85f6c0179fcceaab86eda9c887
dawnot <1537476031@qq.com>
2024-09-25 21:33:01 +0800

    fix: error import

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
-import BlogList from "../../_components/BlogList";
-import "./page.module.css";
 import FadeText from "@/_components/FadeText";
+import Image from "next/image";
+import "./page.module.css";
 
 const description = "这里是一只正在学习前端,渴望学会全栈,梦想做开源的未晓";
 
@@ -9,7 +8,7 @@ export default function Home() {
   return (
     <>
       <main className="pt-40 space-y-5 ">
-        <div className="  mx-auto ">
+        <div className="mx-auto ">
           <Image
             width={100}
             height={100}
